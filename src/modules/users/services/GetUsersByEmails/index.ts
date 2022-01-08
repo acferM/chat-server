87e1422c3fb6b8b0f07ef54b447eec9a1cf26872
acferM <matheus.acfer@nave.org.br>
2022-01-08 14:23:28 -0300

    Updated Get Users by Email Service

--- src/modules/users/services/GetUsersByEmails/index.ts
+++ src/modules/users/services/GetUsersByEmails/index.ts
@@ -1,15 +1,30 @@
 import { inject, injectable } from 'tsyringe';
 
+import { IRequestProvider } from '@shared/container/providers/RequestProvider/models/IRequestProvider';
+
 import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+type GitUser = {
+  email: string;
+};
 
 @injectable()
 class GetUsersByEmailsService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('RequestProvider')
+    private requestProvider: IRequestProvider,
   ) {}
 
-  async execute(emails: string[]) {
+  async execute(emailsUrl: string) {
+    const { data: gitUsers } = await this.requestProvider.get<GitUser[]>(
+      emailsUrl,
+    );
+
+    const emails = gitUsers.map(user => user.email);
+
     const users = await this.usersRepository.findManyByEmails(emails);
 
     return users;
